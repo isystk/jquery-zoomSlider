64d332ae86b37257cf266b0c9012bc2aa231e91a
iseyoshitaka <iseyoshitaka@team-lab.com>
2020-03-01 01:35:14 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+jquery-isystkSlider
+====
+
+## Description
+
+## Demo
+https://demo.isystk.com/jquery-isystkSlider/
+
+## VS. 
+
+## Requirement
+
+## Usage
+
+## Install
+
+``` bash
+# install
+$ yarn install
+
+# static generate
+$ yarn run generate
+
+# server start
+$ yarn run dev
+$ open http://localhost:3000/
+```
+
+## Contribution
+
+## Licence
+
+[MIT](https://github.com/isystk/jquery-isystkSlider/LICENCE)
+
+## Author
+
+[isystk](https://github.com/isystk)
+
+
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import './assets/css/normalize.css';
+import './assets/css/prettify.css';
+import './assets/index.css';
+
+var $ = require('./assets/js/jquery-3.4.1.min.js');
+window.$ = $;
+window.jQuery = $;
+var _ = require('./assets/js/underscore.js');
+window._ = _;
+window.underscore = _;
+require('./assets/js/prettify.js');
+require('./assets/js/lang-css.js');
+require('./assets/js/jquery-stickyNavigator.js');
+require('./assets/js/jquery-isystkOverlay.js');
+require('./assets/js/jquery-isystkSlider.js');
+require('./assets/js/jquery-zoomSlider.js');
+require('./assets/js/common.js');
